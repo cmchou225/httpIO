5f129acbdeb0cb7a19ecfa2b94879bb03b3a519a
Charles <cming.chou@gmail.com>
2017-04-10 03:59:56 +0000

    step 5

--- https_example.js
+++ https_example.js
@@ -3,12 +3,13 @@ const https = require('https');
 console.log('I did it!');
 
 const options = {
-host: 'www.example.com', 
+host: 'www.example.org', 
 path: '/'
 };
 
-const callback = function (){
-console.log('In response to handler callback!');
+const callback = function (response){
+  console.log('In response to handler callback!');
+  console.log('response ', response);
 }
 
 console.log("i'm about to make the request!");
